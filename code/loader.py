583be3638e6817c11584ca4fa4f69b92b1d1389a
Philipp-Ro <philipp.rosin@gmail.com>
2023-04-21 16:10:20 +0200

    new immunohistology  normalization

--- code/loader.py
+++ code/loader.py
@@ -31,8 +31,8 @@ class stain_transfer_dataset(Dataset):
 
 
     def __getitem__(self, idx):
-        HE_img= load_image_to_tensor(idx= idx, folder_dir=self.HE_img_dir, img_names=self.img_names, preprocess_list=self.preprocess_HE)
-        IHC_img = load_image_to_tensor(idx= idx, folder_dir=self.IHC_img_dir, img_names=self.img_names, preprocess_list=self.preprocess_IHC)
+        HE_img= load_image_to_tensor(idx= idx, folder_dir=self.HE_img_dir, img_names=self.img_names, preprocess_list=self.preprocess_HE, img_class='HE')
+        IHC_img = load_image_to_tensor(idx= idx, folder_dir=self.IHC_img_dir, img_names=self.img_names, preprocess_list=self.preprocess_IHC, img_class='IHC')
 
         HE_img = HE_img.cuda()
         IHC_img = IHC_img.cuda()
@@ -41,27 +41,26 @@ class stain_transfer_dataset(Dataset):
         IHC_patches = IHC_img.unfold(1, self.img_size, self.img_size).unfold(2, self.img_size, self.img_size)
 
         # reshape the images 
-
         HE_tensor = reshape_img(HE_patches, self.img_size, self.img_patch)
         IHC_tensor = reshape_img(IHC_patches, self.img_size, self.img_patch)
        
         return HE_tensor,  IHC_tensor, self.img_names[idx]
 
 
-def load_image_to_tensor(idx, folder_dir, img_names,preprocess_list):
+def load_image_to_tensor(idx, folder_dir, img_names, preprocess_list, img_class):
     img_path = os.path.join(folder_dir, img_names[idx])
     pil_img = Image.open(img_path)
     #img_tensor = fn.to_tensor(pil_img)
-    img_tensor, skip_token = preprocess_img(preprocess_list, pil_img)
+    img_tensor, skip_token = preprocess_img(preprocess_list, pil_img, img_class)
   
-
     if skip_token==1:
         img_path = os.path.join(folder_dir, img_names[idx+1])
         pil_img = Image.open(img_path)
         img_tensor = fn.to_tensor(pil_img)
-        img_tensor, skip_token = preprocess_img(preprocess_list, img_tensor)
+        img_tensor, skip_token = preprocess_img(preprocess_list, img_tensor, img_class)
 
     return img_tensor
+
 
 def reshape_img(img, img_size, img_patch):
     num_patches = (1024 * 1024) // img_size**2 
@@ -70,7 +69,8 @@ def reshape_img(img, img_size, img_patch):
     img = img[img_patch,:,:,:]
     return img
 
-def preprocess_img(preprocess_list, img_tensor):
+def preprocess_img(preprocess_list, img_tensor, img_class):
+
     transform_list = []
     skip_token = 0
     seed = np.random.randint(2147483647) 
@@ -78,17 +78,32 @@ def preprocess_img(preprocess_list, img_tensor):
     torch.manual_seed(seed)
     transform_list.append(transforms.ToTensor())
     # check the preprocess list and add the different transforms 
-    if "normalise" in preprocess_list:
+    if "normalise_img_net" in preprocess_list:
         #imagenet mean and std
-        mean = [0.485, 0.456, 0.406]  
+        mean = [0.485, 0.456, 0.406] 
         std = [0.229, 0.224, 0.225]
-        if mean == 0 or std == 0:
-            skip_token = 1
-            img_preprocessed = []
+
+        transform_list.append(transforms.Normalize(mean, std))
+
+    elif "normalise_bci_data" in preprocess_list and img_class == 'HE':
+        # the mean and std where calculated from the train data of the bci data set 
+        # to gain specific mean and std for immunohistological images 
+        mean = [0.6374934061876859, 0.54221270388668, 0.6263932794073621]
+        std = [0.08630574808474256, 0.09923751557584395, 0.07704577263561006]
+
+        transform_list.append(transforms.Normalize(mean, std))
+
+    elif "normalise_bci_data" in preprocess_list and img_class == 'IHC':
+        # the mean and std where calculated from the train data of the bci data set 
+        # to gain specific mean and std for immunohistological images 
+        mean = [0.7687480146058773, 0.7460195554334789, 0.7200858060498716]
+        std = [0.05067490897275364, 0.05804296102376095, 0.06596264508250156]
+
         transform_list.append(transforms.Normalize(mean, std))
 
     if "colorjitter" in preprocess_list:
         transform_list.append(transforms.ColorJitter(brightness=(0.7,1), contrast = (1,3), saturation=(0.7,1.3), hue=(-0.1,0.1)))
+
     if "grayscale" in preprocess_list:
          transform_list.append(transforms.Grayscale(3))
 
